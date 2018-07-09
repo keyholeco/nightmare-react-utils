0f5a9a5c7ae8fd39fd1cd705a23f481ea8d362fe
Hussain Abbas <habbas@keyhole.co>
2018-07-09 13:06:20 -0400

    Edit to give preference to main fiberNode return.

--- src/actions.js
+++ src/actions.js
@@ -46,14 +46,20 @@ const find = function (selector, done) {
         const fiberNode = element[key]
         if (fiberNode && fiberNode.return && (fiberNode.return.stateNode || fiberNode.return.return.stateNode)) {
           if (
+            (fiberNode.return.stateNode.props || fiberNode.return.stateNode.state) &&
+            Object.keys(fiberNode.return.stateNode).length > 0
+          ) {
+            const {props, state, context} = fiberNode.return.stateNode
+            if (props && state && context) return { props, state, context }
+          }
+
+          if (
             (!fiberNode.return.stateNode.props || !fiberNode.return.stateNode.state) &&
-            Object.keys(fiberNode.return.return.stateNode).length > -1
+            Object.keys(fiberNode.return.return.stateNode).length > 0
           ) {
             const {props, state, context} = fiberNode.return.return.stateNode
             if (props && state && context) return { props, state, context }
           }
-          const {props, state, context} = fiberNode.return.stateNode
-          if (props && state && context) return { props, state, context }
         }
       }
     }
@@ -84,14 +90,20 @@ const findAll = function (selector, done) {
           const fiberNode = element[key]
           if (fiberNode && fiberNode.return && (fiberNode.return.stateNode || fiberNode.return.return.stateNode)) {
             if (
+              (fiberNode.return.stateNode.props || fiberNode.return.stateNode.state) &&
+              Object.keys(fiberNode.return.stateNode).length > 0
+            ) {
+              const {props, state, context} = fiberNode.return.stateNode
+              if (props && state && context) return { props, state, context }
+            }
+
+            if (
               (!fiberNode.return.stateNode.props || !fiberNode.return.stateNode.state) &&
               Object.keys(fiberNode.return.return.stateNode).length > -1
             ) {
               const {props, state, context} = fiberNode.return.return.stateNode
               if (props && state && context) return { props, state, context }
             }
-            const {props, state, context} = fiberNode.return.stateNode
-            if (props && state && context) return { props, state, context }
           }
         }
       }
@@ -217,14 +229,19 @@ function waitelem (self, selector, done) {
     '      const fiberNode = element[key];' +
     '      if (fiberNode && fiberNode.return && (fiberNode.return.stateNode || fiberNode.return.return.stateNode)) {' +
     '        if (' +
+    '          (fiberNode.return.stateNode.props || fiberNode.return.stateNode.state) &&' +
+    '          Object.keys(fiberNode.return.stateNode).length > 0' +
+    '        ) {' +
+    '          const {props, state, context} = fiberNode.return.stateNode' +
+    '          if (props && state && context) return { props, state, context }' +
+    '        }' +
+    '        if (' +
     '          (!fiberNode.return.stateNode.props || !fiberNode.return.stateNode.state) &&' +
     '          Object.keys(fiberNode.return.return.stateNode).length > -1' +
     '        ) {' +
     '          const {props, state, context} = fiberNode.return.return.stateNode;' +
     '          if (props && state && context) return { props, state, context };' +
     '        }' +
-    '        const {props, state, context} = fiberNode.return.stateNode;' +
-    '        if (props && state && context) return { props, state, context };' +
     '      }' +
     '    }' +
     '  }' +
